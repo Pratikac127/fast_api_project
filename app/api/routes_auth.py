6f52735d4f2cec4739e6e1abe32ec2ab4a2a51be
pratikac127 <pratikac127@gmail.com>
2026-06-11 16:43:14 +0530

    Add initial API routes, dependencies, and exception handling

---
+++ app/api/routes_auth.py
@@ -0,0 +1,21 @@
+from fastapi import APIRouter
+from pydantic import BaseModel
+from app.core.security import create_access_token
+
+
+router  = APIRouter()
+
+class AuthInput(BaseModel):
+    username :str
+
+    password: str
+
+@router.post()
+def login(auth: AuthInput):
+    if auth.username == "admin" and auth.password == "admin":
+        token = create_access_token({'sub': auth.username})
+        return {"access_token": token}
+    else: return {'invalid credentials'}
+
+
+
