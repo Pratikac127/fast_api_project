a590c23ca2a68dafde732f752e7ec1b0ecf2eb1d
pratikac127 <pratikac127@gmail.com>
2026-06-11 19:46:09 +0530

    Add prediction endpoint and logging middleware; update requirements

---
+++ app/main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from prometheus_fastapi_instrumentator import Instrumentator
+from app.api import routes_auth , route_predict
+from app.middlewares.logging_middleware import LoggingMiddleware
+from app.core.exeptions import register_exception_handler
+
+
+app = FastAPI(title="car_price_prediction_api")
+
+
+
+app.add_middleware(LoggingMiddleware)
+
+app.include_router(routes_auth.router)
+
+app.include_router(route_predict.api_router)
+
+Instrumentator.instrument(app).expose(app)
+
+register_exception_handler(app)
